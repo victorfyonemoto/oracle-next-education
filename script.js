f5ae61d86b14feb0576d5e689d54e6152e149241
Victor Ferreira Yonemoto <82904762+victorfyonemoto@users.noreply.github.com>
2024-02-02 14:41:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+/* || TIME */
+
+const year = document.getElementById("year");
+const thisYear = new Date().getFullYear();
+
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
+
+/* || THEME */
+
+const root = document.querySelector(':root');
+const checkbox = document.querySelector('.theme');
+const logo = document.querySelector('.logo');
+
+checkbox.addEventListener('change', function() {
+    if(this.checked) {
+        root.setAttribute('class','light');
+        logo.src = './img/chat-regular-black.png';
+    } else {
+        root.removeAttribute('class', 'light');
+        logo.src = './img/chat-regular-white.png';
+    }
+});
+
+/* || TEXT */
+
+const textArea = document.querySelector(".text");
+const resultText = document.querySelector(".result__text");
+
+function btnEncrypt() {
+    const encryptedText = encrypt(textArea.value);
+    resultText.value = encryptedText;
+    textArea.value = "";
+}
+
+function encrypt(encryptedString) {
+
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"], ["a" ,"ai"] , ["o" , "ober"], ["u" , "ufat"]];
+    
+    encryptedString = encryptedString.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(encryptedString.includes(matrizCodigo[i][0])) {
+            encryptedString = encryptedString.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return encryptedString;
+}
+
+
+function btnDecrypt() {
+    const decryptedText = decrypt(textArea.value);
+    resultText.value = decryptedText;
+    textArea.value = "";
+}
+
+
+function decrypt(decryptedString) {
+
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"], ["a" ,"ai"] , ["o" , "ober"], ["u" , "ufat"]];
+    decryptedString = decryptedString.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(decryptedString.includes(matrizCodigo[i][1])) {
+            decryptedString = decryptedString.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    return decryptedString;
+}
+
+const copyContent = async () => {
+    try {
+        if(resultText.value.length != 0){
+            await navigator.clipboard.writeText(resultText.value);
+            alert('Conteúdo copiado para a área de transferência');
+        }
+        else{
+            alert('Nenhum texto para copiar!')
+        }
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  }
